a1bfefb7cba84da47729af26c9f34ea47a786c7a
Eve Amber Goodmon <eveambergoodmon@Eves-MacBook-Air.local>
2023-02-16 10:39:45 +0700

    Memodifikasi Tugas_6_Algo2.java

--- Method and Class/src/Tugas_6_Algo2.java
+++ Method and Class/src/Tugas_6_Algo2.java
@@ -34,9 +34,9 @@ public class Tugas_6_Algo2 {
 
     public static void main (String[] args) {
 
-        Belanjaan costumer1 = new Belanjaan (10000000);
-        Belanjaan costumer2 = new Belanjaan (750000);
-        Belanjaan costumer3 = new Belanjaan (5000000);
+        Belanjaan costumer1 = new Belanjaan (10000000, "", "");
+        Belanjaan costumer2 = new Belanjaan (750000, "", "");
+        Belanjaan costumer3 = new Belanjaan (5000000,  "", "");
 
         System.out.println ("Se");
 
